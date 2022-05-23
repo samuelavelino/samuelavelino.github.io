0697540af82c00e89265a009a433434446f2abcd
Samuel Avelino <48601223+samuelavelino@users.noreply.github.com>
2022-05-23 15:43:18 -0300

    Add files via upload

---
+++ mugen/chars/chunlines/chunlines.cmd
@@ -0,0 +1,865 @@
+;--------------------------------------------------------------------------
+;Keys (Change to keyboard config or joy config)
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "hold_x"
+command = /x
+
+[Command]
+name = "hold_y"
+command = /y
+
+;-| Super Motions |--------------------------------------------------------
+[Command]
+name = "Lightning Ciclone Kick"
+command = ~D, DB, B, D, DB, B,b+y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Lightning Ciclone Kick"
+command = ~D, B, D, B,b+y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Shin Kikoushou"
+command = ~D, DF, F, D, DF, F,b+y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Shin Kikoushou"
+command = ~D, F, D, F,b+y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Hazan Tensho Kyaku"
+command = ~D, B, D, B, ~a
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Hazan Tensho Kyaku"
+command = ~D, B, D, B, a
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Hazan Tensho Kyaku"
+command = ~D, B, D, B, b
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Hazan Tensho Kyaku"
+command = ~D, B, D, B, ~b
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Kikoushou"
+command = ~D, DF, F, D, DF, F, x
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Kikoushou"
+command = ~D, F, D, F, x
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Kikoushou"
+command = ~D, DF, F, D, DF, F, y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Kikoushou"
+command = ~D, F, D, F, y
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Senretsu Kyaku"
+command = ~D, F, D, F, ~a
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Senretsu Kyaku"
+command = ~D, F, D, F, a
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Senretsu Kyaku"
+command = ~D, F, D, F, b
+time = 30
+buffer.time = 5
+
+[Command]
+name = "Senretsu Kyaku"
+command = ~D, F, D, F, ~b
+time = 30
+buffer.time = 5
+
+;-| Special Motions |------------------------------------------------------
+[Command]
+name = "Weak Shoulder Bash"
+command = ~D, B, x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Shoulder Bash"
+command = ~D, B, ~x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Shoulder Bash"
+command = ~D, B, y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Shoulder Bash"
+command = ~D, B, ~y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Kikouken"
+command = ~D, F, x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Kikouken"
+command = ~D, F, ~x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Kikouken"
+command = ~D, F, y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Kikouken"
+command = ~D, F, ~y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Fierce Kikouken"
+command = ~D, F, z
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Fierce Kikouken"
+command = ~D, F, ~z
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Palm Strike"
+command = ~30$B, F, x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Palm Strike"
+command = ~30$B, F, ~x
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Palm Strike"
+command = ~30$B, F, y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Palm Strike"
+command = ~30$B, F, ~y
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Lightning Kick"
+command = ~F, B, F, a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Lightning Kick"
+command = ~F, B, F, ~a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Lightning Kick"
+command = ~F, B, F, b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Lightning Kick"
+command = ~F, B, F, ~b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Ciclone Kick"
+command = ~D, B, a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Ciclone Kick"
+command = ~D, B, ~a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Ciclone Kick"
+command = ~D, B, b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Ciclone Kick"
+command = ~D, B, ~b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Classic Ciclone Kick"
+command = ~30$B, F, a
+time = 40
+buffer.time = 3
+
+[Command]
+name = "Weak Classic Ciclone Kick"
+command = ~30$B, F, ~a
+time = 40
+buffer.time = 3
+
+[Command]
+name = "Strong Classic Ciclone Kick"
+command = ~30$B, F, b
+time = 40
+buffer.time = 3
+
+[Command]
+name = "Strong Classic Ciclone Kick"
+command = ~30$B, F, ~b
+time = 40
+buffer.time = 3
+
+[Command]
+name = "Weak Air Kick"
+command = ~D, F, a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Air Kick"
+command = ~D, F, ~a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Air Kick"
+command = ~D, F, b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Air Kick"
+command = ~D, F, ~b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Fierce Ciclone Kick"
+command = ~D, B, c
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Fierce Ciclone Kick"
+command = ~D, B, ~c
+time = 20
+buffer.time = 3
+
+
+[Command]
+name = "Weak Rising Kick"
+command = ~F, D, F, a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Weak Rising Kick"
+command = ~F, D, F, ~a
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Rising Kick"
+command = ~F, D, F, b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Strong Rising Kick"
+command = ~F, D, F, ~b
+time = 20
+buffer.time = 3
+
+[Command]
+name = "Spinning Pile Drive"
+command = ~F, B, F, x
+time=20
+buffer.time = 7
+
+[Command]
+name = "Spinning Pile Drive"
+command = ~F, B, F, ~x
+time=20
+buffer.time = 7
+
+[Command]
+name = "Spinning Pile Drive"
+command = ~F, B, F, y
+time=20
+buffer.time = 7
+
+[Command]
+name = "Spinning Pile Drive"
+command = ~F, B, F, ~y
+time=20
+buffer.time = 7
+
+[Command]
+name = "Shoulder Slam"
+command = z
+
+[Command]
+name = "Shoulder Slam"
+command = ~z
+
+[Command]
+name = "Flip Kick"
+command = c
+
+[Command]
+name = "Flip Kick"
+command = ~c
+
+[Command]
+name = "Head Stomp"
+command = /D,a
+
+[Command]
+name = "negative x"
+command = ~x
+
+[Command]
+name = "negative y"
+command = ~y
+
+;-| Double Tap |-----------------------------------------------------------
+[Command]
+name = "FF"     ;Required (do not remove)
+command = F, F
+time = 10
+
+[Command]
+name = "BB"     ;Required (do not remove)
+command = B, B
+time = 10
+
+;-| 2/3 Button Combination |-----------------------------------------------
+[Command]
+name = "recovery";Required (do not remove)
+command = a+x
+time = 1
+
+[Command]
+name = "start"
+command = s
+time = 1
+
+;-| Hold Dir |--------------------------------------------------------------
+[Command]
+name = "holdfwd";Required (do not remove)
+command = /$F
+time = 1
+
+[Command]
+name = "holdback";Required (do not remove)
+command = /$B
+time = 1
+
+[Command]
+name = "holdup" ;Required (do not remove)
+command = /$U
+time = 1
+
+[Command]
+name = "holddown";Required (do not remove)
+command = /$D
+time = 1
+
+[Command]
+name = "DiagFwdUp"
+command = /UF
+
+[Command]
+name = "DiagBckUp"
+command = /UB
+
+;===========================================================================
+;---------------------------------------------------------------------------
+[Statedef -1]
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Desesperation Attacks
+
+;---------------------------------------------------------------------------
+;Lightning Ciclone Kick
+[State -1, Lightning Ciclone Kick]
+type = ChangeState
+triggerall = (var(55) & 4096)
+;triggerall = var(39) = 0
+;triggerall = life <= floor(LifeMax / 2.0 + 0.5)
+triggerall = power >= 3000
+trigger1 = (command = "Lightning Ciclone Kick")
+value = 3300 + 50 * (statetype = A)
+
+;---------------------------------------------------------------------------
+;Shin Kikoushou
+[State -1, Shin Kikoushou]
+type = ChangeState
+triggerall = statetype != A
+triggerall = (var(55) & 4096)
+triggerall = power >= 3000
+trigger1 = (command = "Shin Kikoushou")
+value = 3400
+
+;---------------------------------------------------------------------------
+;Kikoushou
+[State -1, Kikoushou]
+type = ChangeState
+triggerall = statetype != A
+triggerall = (var(55) & 8192)
+triggerall = power >= 1000
+triggerall = statetype != A
+trigger1 = (command = "Kikoushou")
+value = 3000
+
+;---------------------------------------------------------------------------
+;Senretsu Kyaku
+[State -1, Senretsu Kyaku]
+type = ChangeState
+triggerall = (var(55) & 32768)
+triggerall = power >= 1000
+triggerall = statetype != A
+trigger1 = (command = "Senretsu Kyaku")
+value = 3100; + 50 * (statetype = A)
+
+;---------------------------------------------------------------------------
+;Hazan Tensho Kyaku
+[State -1, Hazan Tensho Kyaku]
+type = ChangeState
+triggerall = (var(55) & 16384)
+triggerall = power >= 1000
+trigger1 = (command = "Hazan Tensho Kyaku")
+value = 3200
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Special Attacks
+
+;---------------------------------------------------------------------------
+;Spinning Pile Drive
+[State -1, Spinning Pile Drive]
+type = ChangeState
+triggerall = StateType != A
+triggerall = (var(55) & 1024)
+trigger1 = (command = "Spinning Pile Drive")
+value = 1300
+
+;---------------------------------------------------------------------------
+;Lightning Kick
+[State -1, Lightning Kick]
+type = ChangeState
+triggerall = StateType != A
+triggerall = (var(55) & 256)
+trigger1 = (command = "Weak Lightning Kick")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Lightning Kick")
+trigger2 = (var(50) := 2)
+value = 1100
+
+;---------------------------------------------------------------------------
+;Rising Kick
+[State -1, Hadouken]
+type = ChangeState
+triggerall = (var(55) & 1024)
+triggerall = statetype != A
+trigger1 = (command = "Weak Rising Kick")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Rising Kick")
+trigger2 = (var(50) := 2)
+value = 1400
+
+;---------------------------------------------------------------------------
+;Shoulder Bash
+[State -1, Shoulder Bash]
+type = ChangeState
+triggerall = (var(55) & 2048)
+triggerall = statetype != A
+trigger1 = (command = "Weak Shoulder Bash")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Shoulder Bash")
+trigger2 = (var(50) := 2)
+value = 1500
+
+;---------------------------------------------------------------------------
+;Palm Strike
+[State -1, Palm Strike]
+type = ChangeState
+triggerall = (var(55) & 128)
+triggerall = statetype != A
+trigger1 = (command = "Weak Palm Strike")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Palm Strike")
+trigger2 = (var(50) := 2)
+value = 1800
+
+;---------------------------------------------------------------------------
+;Kikouken
+[State -1, Kikouken]
+type = ChangeState
+triggerall = (var(55) & 128)
+triggerall = numhelper(1000) = 0
+triggerall = statetype != A
+trigger1 = (command = "Weak Kikouken")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Kikouken")
+trigger2 = (var(50) := 2)
+value = 1000; + 60 * (statetype = A)
+
+;---------------------------------------------------------------------------
+;Kikouken
+[State -1, Kikouken]
+type = ChangeState
+triggerall = (var(55) & 128)
+triggerall = statetype != A
+triggerall = numhelper(1000) = 0
+triggerall = power >= 200
+trigger1 = (command = "Fierce Kikouken")
+value = 1050
+
+;---------------------------------------------------------------------------
+;Ciclone Kick
+[State -1, Ciclone Kick]
+type = ChangeState
+triggerall = (var(55) & 512)
+trigger1 = (command = "Weak Ciclone Kick")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Ciclone Kick")
+trigger2 = (var(50) := 2)
+value = 1200 + (statetype = A)
+
+;---------------------------------------------------------------------------
+;Classic Ciclone Kick
+[State -1, Ciclone Kick]
+type = ChangeState
+triggerall = (var(55) & 512)
+triggerall = statetype != A
+trigger1 = (command = "Weak Classic Ciclone Kick")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Classic Ciclone Kick")
+trigger2 = (var(50) := 2)
+value = 1270
+
+;---------------------------------------------------------------------------
+;Air Kick
+[State -1, Ciclone Kick]
+type = ChangeState
+triggerall = (var(55) & 512)
+triggerall = statetype = A
+triggerall = pos y < -15
+triggerall = stateno != [1700,1705]
+trigger1 = (command = "Weak Air Kick")
+trigger1 = (var(50) := 1)
+trigger2 = (command = "Strong Air Kick")
+trigger2 = (var(50) := 2)
+value = 1700
+
+;---------------------------------------------------------------------------
+;Ciclone Kick
+[State -1, Ciclone Kick]
+type = ChangeState
+triggerall = (var(55) & 512)
+triggerall = power >= 200
+trigger1 = statetype != A
+trigger1 = (command = "Fierce Ciclone Kick")
+value = 1250
+
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Run Fwd
+[State -1, Run Fwd]
+type = ChangeState
+value = 100
+trigger1 = command = "FF"
+trigger1 = statetype = S
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Run Back
+[State -1, Run Back]
+type = ChangeState
+value = 105
+trigger1 = command = "BB"
+trigger1 = statetype = S
+trigger1 = ctrl
+
+;===========================================================================
+;Normal Attacks
+
+;---------------------------------------------------------------------------
+;Evade
+[State -1, Zero Counter]
+type = ChangeState
+triggerall = stateno = 0 || stateno = 20
+trigger1 = (command = "recovery")
+value = 2200
+
+;---------------------------------------------------------------------------
+;Shoulder Slam Start
+[State -1, Shoulder Slam]
+type = ChangeState
+triggerall = statetype != C
+triggerall = Stateno != 100
+triggerall = var(55) & 4
+trigger1 = (command = "Shoulder Slam")
+trigger1 = (var(50) := 1)
+value = 800 + 9 * (statetype = A)
+
+;---------------------------------------------------------------------------
+;Shoulder Slam Start
+[State -1, Shoulder Slam]
+type = ChangeState
+triggerall = statetype = A
+triggerall = Stateno != 100
+triggerall = var(55) & 4
+trigger1 = (command = "Flip Kick")
+trigger1 = (var(50) := 1)
+value = 830
+
+;---------------------------------------------------------------------------
+;Shoulder Slam Start
+[State -1, Shoulder Slam]
+type = ChangeState
+triggerall = statetype = S
+triggerall = Stateno != 100
+triggerall = var(55) & 4
+trigger1 = (command = "Flip Kick")
+trigger1 = (var(50) := 1)
+value = 816
+
+;---------------------------------------------------------------------------
+;Near Stand Weak Punch
+[State -1, Near Stand Weak Punch]
+type = ChangeState
+trigger1 = statetype = S
+trigger1 = (command != "holddown")
+trigger1 = (var(55) & 1)
+trigger1 = (command = "x")
+trigger1 = P2BodyDist X < 9
+value = 205
+
+;---------------------------------------------------------------------------
+;Stand/Crouch Light Punch
+[State -1, Stand Light Punch]
+type = ChangeState
+triggerall = statetype != A
+trigger1 = (var(55) & 1)
+trigger1 = (command = "x")
+value = 200 + (200 * Command = "holddown")
+
+;---------------------------------------------------------------------------
+;Near Stand Strong Punch
+[State -1, Near Stand Strong Punch]
+type = ChangeState
+trigger1 = statetype = S
+trigger1 = (command != "holddown")
+trigger1 = (var(55) & 8)
+trigger1 = (command = "y")
+trigger1 = P2BodyDist X = [-12,12]
+value = 225
+
+;---------------------------------------------------------------------------
+;Stand/Crouch Strong Punch
+[State -1, Stand/Crouch Strong Punch]
+type = ChangeState
+triggerall = statetype != A
+trigger1 = (var(55) & 8)
+trigger1 = (command = "y")
+value = 220 + (200 * Command = "holddown")
+
+;---------------------------------------------------------------------------
+;Stand/Crouch Light Kick
+[State -1, Stand/Crouch Light Kick]
+type = ChangeState
+triggerall = statetype != A
+trigger1 = (var(55) & 2)
+trigger1 = (command = "a")
+value = 210 + (200 * Command = "holddown")
+
+;---------------------------------------------------------------------------
+;Near Stand Strong Kick
+[State -1, Near Stand Strong Punch]
+type = ChangeState
+trigger1 = statetype = S
+trigger1 = (command != "holddown")
+trigger1 = (var(55) & 8)
+trigger1 = (command = "b")
+trigger1 = P2BodyDist X = [-12,12]
+value = 235
+
+;---------------------------------------------------------------------------
+;Stand/Crouch Strong Kick
+[State -1, Stand/Crouch Strong Punch]
+type = ChangeState
+triggerall = statetype != A
+trigger1 = (var(55) & 16)
+trigger1 = (command = "b")
+value = 230 + (200 * Command = "holddown")
+
+;---------------------------------------------------------------------------
+;Taunt
+[State -1, Taunt]
+type = ChangeState
+value = 195
+triggerall = command = "start"
+trigger1 = statetype != A
+trigger1 = ctrl
+trigger1 = var(41) = 0
+
+;---------------------------------------------------------------------------
+;Jump Weak Punch
+[State -1, Jump Weak Punch]
+type = ChangeState
+trigger1 = (var(55) & 32)
+trigger1 = (command = "x")
+value = 600
+
+;---------------------------------------------------------------------------
+;Head Stomp
+[State -1, Head Stomp]
+type = ChangeState
+trigger1 = statetype = A
+trigger1 = (var(55) & 4)
+trigger1 = (command = "Head Stomp")
+value = 615
+
+;---------------------------------------------------------------------------
+;Jump Weak Kick
+[State -1, Jump Weak Kick]
+type = ChangeState
+trigger1 = (var(55) & 32)
+trigger1 = (command = "a")
+value = 610
+
+;---------------------------------------------------------------------------
+;Jump Strong Punch
+[State -1, Jump Strong Punch]
+type = ChangeState
+trigger1 = (var(55) & 64)
+trigger1 = (command = "y")
+value = 620
+
+;---------------------------------------------------------------------------
+;Jump Strong Kick
+[State -1, Jump Strong Kick]
+type = ChangeState
+trigger1 = (var(55) & 64)
+trigger1 = (command = "b")
+value = 630
+
+;--------------------------------------------------------------------------
+;Triangle Jump
+[state -1, Triangle Jump]
+Type = ChangeState
+triggerall = StateType = A
+triggerall = ctrl
+trigger1 = vel x < 0
+trigger1 = BackEdgeDist < 10
+trigger1 = command = "DiagFwdUp"
+trigger2 = vel x > 0
+trigger2 = FrontEdgeDist < 10
+trigger2 = command = "DiagBckUp"
+value = 45
+
+;---------------------------------------------------------------------------
+;Emergency Escape
+[State -1, Zero Counter]
+type = ChangeState
+triggerall = (var(55) & 131072)
+triggerall = (command = "start")
+trigger1 = (command = "holdfwd")
+triggerall = power >= 250
+value = 2100
+
+;---------------------------------------------------------------------------
+;Zero Counter
+[State -1, Zero Counter]
+type = ChangeState
+trigger1 = (var(55) & 131072)
+trigger1 = (command = "start")
+triggerall = power >= 250
+value = 2000
