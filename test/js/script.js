e034b835567ed279e22454d8d77e706d43ea1c1a
Samuel Avelino <48601223+samuelavelino@users.noreply.github.com>
2022-08-05 02:40:39 -0300

    Update script.js

--- test/js/script.js
+++ test/js/script.js
@@ -1,3 +1,9 @@
-﻿document.getElementById("text").innerHTML = "Hello world!";
-const myHeading = document.querySelector('h1');
+var now = new Date();
+var day = ('0' + now.getDate()).slice(-2);
+var month = ('0' + (now.getMonth() + 1)).slice(-2);
+var today = now.getFullYear()+'-'+(month)+'-'+(day) ;
+document.title = today;
+
+document.getElementById('text').innerHTML = 'Hello world!';
+const myHeading = document.querySelector('div');
 myHeading.textContent = 'Under Construction!';
